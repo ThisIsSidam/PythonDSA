25a899ddb9759eff54f95dd2f1be0c752efa866f
Sidam <thisissidam@gmail.com>
2023-10-18 19:33:06 +0530

    Finished the entire LiskedList class, from constructor to reverse method.

---
+++ LinkedList/main.py
@@ -0,0 +1,144 @@
+class Node:
+    def __init__ (self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__ (self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+    
+    def print_list (self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next 
+
+    def append (self, value):
+        new_node = Node(value)
+
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            self.tail = new_node
+
+        self.length += 1
+        return True
+
+    def prepend (self, value):
+        new_node = Node(value)
+         
+        if self.length == 0:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        
+        self.length += 1
+        return True
+
+    def pop(self):
+        if self.length == 0:
+            return None
+        elif self.length == 1:
+            temp = self.head
+            self.head = None
+            self.tail = None
+        else:
+            pre = self.head
+            while pre.next.next is not None:
+                pre = pre.next
+            temp = pre.next
+            self.tail = pre
+            self.tail.next = None
+        self.length -= 1
+        return temp
+    
+    def pop_first(self):
+        if self.length == 0:
+            return None
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        else:
+            self.head = self.head.next
+            temp.next = None
+        self.length -= 1
+        return temp
+            
+    def get(self, index):
+        if index >= self.length or index < 0:
+            return None
+        
+        temp = self.head
+        for _ in range(index):
+            temp = temp.next
+        return temp
+    
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index > self.length or index < 0:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index >= self.length or index < 0:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length -1:
+            return self.pop()
+        
+        pre = self.get(index -1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+        after = temp.next
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+    
+
+if __name__ == "__main__":
+    ll = LinkedList(5)
+    ll.prepend(4)
+    ll.prepend(6)
+    # ll.pop() 
+
+    ll.reverse()
+    ll.print_list()
+    
